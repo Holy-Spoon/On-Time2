a592994d1d1680c83f57aafbf28245dbdfd37e0f
MatteyoMcGowan <mcgowanm1551@gmail.com>
2025-02-25 00:30:16 +1300

    Calculate Dimensions

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+window.onload =  {
 
-public static void main(String[] args)
-{
-    window.onload = function() {
-        var origin = document.getElementById('origin').value;
-        console.log(origin);
-
-        var height = document.getElementById('height').value
-        console.log(height);
-
-        var length = document.getElementById('length').value
-        console.log(length);
-
-        var width = document.getElementById('width').value
-        console.log(width);
-
-        var cost = (height * length * width);
-        console.log(cost);
-    } 
 }
+
+
+function calculateVolume() {
+
+    let height = document.getElementById("height").value;
+    let length = document.getElementById("length").value;
+    let width = document.getElementById("width").value;
+
+    height = parseFloat(height);
+    length = parseFloat(length);
+    width = parseFloat(width);
+
+    if (height == NaN || length == NaN || width == Nan){
+        console.log("Please Enter in valid numbers");
+        return;
+    }
+
+    let volume = height * length * width;
+    console.log(volume);
+} 
+
